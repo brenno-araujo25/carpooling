bbafc6bf03f2a2e64e928fd00b2fedfb6d4e22d6
brenno-araujo25 <brennocaldeira@outlook.com>
2024-12-26 21:46:41 -0300

    feat: carrega as variáveis de ambiente e as usa em gateway.js

--- api-gateway/src/config/config.js
+++ api-gateway/src/config/config.js
@@ -3,12 +3,15 @@ import path from 'path'
 import { dirname } from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-// carrega as variáveis de ambiente do arquivo .env na raiz do projeto
-const __dirname = dirname(fileURLToPath(import.meta.url))
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+// carrega as variáveis de ambiente quando rodando localmente
+if (process.env.NODE_ENV !== 'production') {
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    dotenv.config({ path: path.resolve(__dirname, '../../../.env') })
+}
 
-if (!process.env.GATEWAY_PORT) {
-    console.error('GATEWAY_PORT is not defined')
+if (!process.env.GATEWAY_PORT || !process.env.USER_SERVICE_URL || 
+    !process.env.AUTH_SERVICE_URL || !process.env.RIDE_SERVICE_URL) {
+    console.error('Variáveis de ambiente não configuradas')
     process.exit(1)
 }
 
@@ -16,5 +19,5 @@ export const config = {
     GATEWAY_PORT: process.env.GATEWAY_PORT,
     USER_SERVICE_URL: process.env.USER_SERVICE_URL,
     AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL,
-    RIDE_SERVICE_URL: process.env.RIDE_SERVICE_URL
+    RIDE_SERVICE_URL: process.env.RIDE_SERVICE_URL,
 }
--- api-gateway/src/gateway.js
+++ api-gateway/src/gateway.js
@@ -1,43 +1,33 @@
 import Fastify from 'fastify'
 import proxy from '@fastify/http-proxy'
 import cors from '@fastify/cors'
-import dotenv from 'dotenv'
-import path from 'path'
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-// carrega as variáveis de ambiente do arquivo .env na raiz do projeto
-const __dirname = dirname(fileURLToPath(import.meta.url))
-dotenv.config({ path: path.resolve(__dirname, '../../.env') })
+import { config } from './config/config.js'
 
 export function createGateway() {
-    if (!process.env.USER_SERVICE_URL || !process.env.AUTH_SERVICE_URL || !process.env.RIDE_SERVICE_URL) {
-        console.error('Por favor, defina as variáveis de ambiente USER_SERVICE_URL, AUTH_SERVICE_URL e RIDE_SERVICE_URL')
-        process.exit(1)
-    }
-
     const fastify = Fastify()
 
     // permite requisições de qualquer origem
     fastify.register(cors)
 
+    const { USER_SERVICE_URL, AUTH_SERVICE_URL, RIDE_SERVICE_URL } = config
+
     // serviço de usuários
     fastify.register(proxy, {
-        upstream: process.env.USER_SERVICE_URL,
+        upstream: USER_SERVICE_URL,
         prefix: '/api/users',
         rewritePrefix: '/users',
     })
 
     // serviço de autenticação
     fastify.register(proxy, {
-        upstream: process.env.AUTH_SERVICE_URL,
+        upstream: AUTH_SERVICE_URL,
         prefix: '/api/auth',
         rewritePrefix: '/auth',
     })
 
     // serviço de caronas
     fastify.register(proxy, {
-        upstream: process.env.RIDE_SERVICE_URL,
+        upstream: RIDE_SERVICE_URL,
         prefix: '/api/rides',
         rewritePrefix: '/rides',
     })
